d39946ad9faad45ee960bf916422adefed21e5dd
Tumonbai <ztima90@gmail.com>
2020-01-12 22:43:44 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Dog dog= new Dog("arslan",18,Color.BLACK,new Owner("Bakyt", "biskek"));
+        System.out.println(dog.getName()+" "+dog.getAge()+" "+dog.getColor());
+        dog.makeVoice();
+
+
+        Haski haski= new Haski("Bekulan",20, Color.BLUE, new Owner("Bakyt", "biskek"));
+        System.out.println(haski.getName()+" "+haski.getAge()+" "+haski.getColor());
+        dog.makeVoice("bakunin");
+
+        Haski haski1= new Haski("Nurbol",28, Color.GREEN,new Owner("Bakyt", "biskek"));
+        System.out.println(haski1.getName()+" "+haski1.getAge()+" "+haski1.getColor());
+        dog.makeVoice("mirbek");
+    }
+
+}
---
+++ src/com/company/Owner.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Owner {
+    private String name;
+
+    public Owner(String name, String adress) {
+        this.name = name;
+        this.adress = adress;
+    }
+
+    private String adress;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+}
